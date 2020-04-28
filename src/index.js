f9db98c36c3c7105b0db4d2887c967bec80201e7
rohit meshram <rohitmeshramd@gmail.com>
2020-04-29 05:26:27 +0530

    add devextreme and bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Game from "./tic-tac-toe/Game";
+// import MyChart from "./my-charts/MyChart";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // ! tic tac toe Renderer
 ReactDOM.render(<Game />, document.getElementById("root"));
+
+// ! chart
+// ReactDOM.render(<MyChart />, document.getElementById("root"));
